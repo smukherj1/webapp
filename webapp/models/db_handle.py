0f676f1d7ed8946772154e1e1d03c29963ec8550
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2017-01-28 13:10:54 -0500

    Change db directory to accessible path

--- webapp/models/db_handle.py
+++ webapp/models/db_handle.py
@@ -3,7 +3,7 @@ db = None
 def init(app):
     from flask_sqlalchemy import SQLAlchemy
     global db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///_webapp_sqlite3.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/suvanjan/apache2/webapp/_sqlite3.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
     db.drop_all()
