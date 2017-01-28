e4f10384bc06f4e97c75181a678db1e4e77f8ed1
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2017-01-28 05:38:08 -0800

    Fix undefined variable access

--- webapp/views/index.py
+++ webapp/views/index.py
@@ -1,11 +1,14 @@
 
+__app = None
 
 def index():
     return '<html><title>Hello!</title><body><h1>Hello!</h1>Howdy! How ya doin?</body></html>'
 
 def favicon():
-    return app.send_static_file('favicon.ico')
+    return __app.send_static_file('favicon.ico')
 
 def init(app):
+    global __app
+    __app = app
     app.add_url_rule('/', 'index', index)
     app.add_url_rule('/favicon.ico', 'favicon', favicon)
