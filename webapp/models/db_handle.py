9a454c0321a4b29725133df6d9311d6e45abbdd9
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2017-01-29 10:32:21 -0500

    Automatically infer and create database directories

--- webapp/models/db_handle.py
+++ webapp/models/db_handle.py
@@ -1,9 +1,23 @@
+import os
+
 db = None
 
 def init(app):
     from flask_sqlalchemy import SQLAlchemy
     global db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/suvanjan/apache2/webapp/_sqlite3.db'
+
+    # Generate db path
+    db_path = os.environ['HOME']
+    db_path = os.path.join(db_path, 'webapp.work')
+    if os.environ.get('WEBAPP_RELEASE') != None:
+        db_path = os.path.join(db_path, 'release', 'database')
+    else:
+        db_path = os.path.join(db_path, 'debug', 'database')
+    # Make directories if they don't exist
+    if not os.path.isdir(db_path):
+        os.makedirs(db_path)
+    db_path = os.path.join(db_path, '_sqlite3.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
     db.drop_all()
